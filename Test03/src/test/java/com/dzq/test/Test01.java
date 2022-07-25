3104dc2001d362188174af19d2bc09d05e9386b1
dzq <x_mitsui@163.com>
2022-07-25 14:25:24 +0800

    MyBatis_查找传参测试

--- Test03/src/test/java/com/dzq/test/Test01.java
+++ Test03/src/test/java/com/dzq/test/Test01.java
@@ -12,7 +12,9 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @Auther: x_mitsui
@@ -22,10 +24,11 @@ import java.util.List;
  */
 public class Test01 {
     private SqlSession sqlSession;
+
     @Before
-    public void aaa(){
+    public void aaa() {
         SqlSessionFactoryBuilder ssfb = new SqlSessionFactoryBuilder();
-        InputStream resourceAsStream=null;
+        InputStream resourceAsStream = null;
         try {
             resourceAsStream = Resources.getResourceAsStream("sqlMapConfig.xml");//找导出目录class下文件
         } catch (IOException e) {
@@ -34,15 +37,81 @@ public class Test01 {
         SqlSessionFactory factory = ssfb.build(resourceAsStream);
         sqlSession = factory.openSession();
     }
+
     @Test
-    public void testFindAll(){
+    public void testFindAll() {
         // MyBatis内部辅助构造了实现类
         EmpMapper empImp = sqlSession.getMapper(EmpMapper.class);
         List<Emp> empList = empImp.findAll();
         empList.forEach(System.out::println);
     }
+
+    @Test
+    public void testFindOne() {
+        EmpMapper empImp = sqlSession.getMapper(EmpMapper.class);
+        Emp emp = empImp.findByEmpNo(7876);
+        System.out.println(emp);
+    }
+
+    @Test
+    public void testFindByMoreParas() {
+        EmpMapper empImp = sqlSession.getMapper(EmpMapper.class);
+        List<Emp> empList = empImp.findEmpByDeptNoAndSalary(20, 1500.0);
+        empList.forEach(System.out::println);
+    }
+
+    @Test
+    public void testFindByMoreParas2() {
+        EmpMapper empImp = sqlSession.getMapper(EmpMapper.class);
+        Map map = new HashMap<>();
+        map.put("depno", 20);
+        map.put("sal", 1500.0);
+        List<Emp> empList = empImp.findEmpByDeptNoAndSalary2(map);
+        empList.forEach(System.out::println);
+    }
+
+    @Test
+    public void testFindByMoreParas3() {
+        EmpMapper empImp = sqlSession.getMapper(EmpMapper.class);
+        Emp emp = new Emp();
+        emp.setDeptNo(20);
+        emp.setSal(1500.0);
+        List<Emp> empList = empImp.findEmpByDeptNoAndSalary3(emp);
+        empList.forEach(System.out::println);
+    }
+
+    @Test
+    public void testFindByMoreParas4() {
+        EmpMapper empImp = sqlSession.getMapper(EmpMapper.class);
+        Emp emp0 = new Emp();
+        emp0.setDeptNo(20);
+        Emp emp1 = new Emp();
+        emp1.setSal(1500.0);
+        List<Emp> empList = empImp.findEmpByDeptNoAndSalary4(emp0, emp1);
+        empList.forEach(System.out::println);
+    }
+
+    @Test
+    public void testFindByMoreParas5() {
+        EmpMapper empImp = sqlSession.getMapper(EmpMapper.class);
+        Emp emp0 = new Emp();
+        emp0.setDeptNo(20);
+        Emp emp1 = new Emp();
+        emp1.setSal(1500.0);
+        List<Emp> empList = empImp.findEmpByDeptNoAndSalary4(emp0, emp1);
+        empList.forEach(System.out::println);
+    }
+
+    @Test
+    public void testFindByName() {
+        EmpMapper empImp = sqlSession.getMapper(EmpMapper.class);
+        String name ="a";
+        List<Emp> empList = empImp.findByName(name);
+        empList.forEach(System.out::println);
+    }
+
     @After
-    public void bbb(){
+    public void bbb() {
         sqlSession.close();
 
     }
